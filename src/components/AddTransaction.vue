7fbdb760a356bd74503bc6f5f0867f76d6ecba59
Gabriel Santos <gabrieljorgesilvasantos@outlook.com>
2024-10-17 22:20:49 -0300

    feat: add rules in components

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,15 +1,35 @@
 <template>
     <h3>Add new transaction</h3>
-    <form id="form">
+    <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Text</label>
-            <input type="text" id="text" placeholder="Enter text..." />
+            <input type="text" id="text" placeholder="Enter text..." v-model="text" />
         </div>
         <div class="form-control">
             <label for="amount">Amount <br />
                 (negative - expense, positive - income)</label>
-            <input type="text" id="amount" placeholder="Enter amount..." />
+            <input type="text" id="amount" placeholder="Enter amount..." v-model="amount" />
         </div>
         <button class="btn">Add transaction</button>
     </form>
 </template>
+
+<script setup>
+import { ref } from "vue"
+import { useToast } from "vue-toastification"
+
+const text = ref('')
+const amount = ref('')
+
+const toast = useToast()
+
+const onSubmit = () => {
+    if (!text.value || !amount.value) {
+        toast.error('Both fields must be filled')
+        return
+    }
+
+    text.value = ''
+    amount.value = ''
+}
+</script>
